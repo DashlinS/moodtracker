11d41b17cfd1f1b84f00743b1a761d3c20e36458
dashlin <dashlin.sermeil@gmail.com>
2021-03-27 15:16:48 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://dashlin:sermeil15@dashbase.olf7t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'dbName': 'moodDB'
+};
